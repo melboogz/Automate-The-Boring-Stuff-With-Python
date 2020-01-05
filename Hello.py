dc188ba23316974015fc9f3d1789872e1f668a4a
Melvin Felix <53129990+melboogz@users.noreply.github.com>
2020-01-05 08:55:35 -0500

    Add files via upload
    Hello World

---
+++ Hello.py
@@ -0,0 +1,29 @@
+Python 3.8.0 (v3.8.0:fa919fdf25, Oct 14 2019, 10:23:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: /Users/z3r0xpl01tz/Desktop/Hello.py =================
+Hello World
+What is your name?
+Bob
+It is gppd tp meet you,Bob
+The length of your name is:
+3
+What is your age?
+25
+Traceback (most recent call last):
+  File "/Users/z3r0xpl01tz/Desktop/Hello.py", line 11, in <module>
+    print("You will be " + string(int(myAge) + 1) + " in a year.")
+NameError: name 'string' is not defined
+>>> 
+================= RESTART: /Users/z3r0xpl01tz/Desktop/Hello.py =================
+Hello World
+What is your name?
+bob
+It is gppd tp meet you,bob
+The length of your name is:
+3
+What is your age?
+25
+You will be 26 in a year.
+>>> 
